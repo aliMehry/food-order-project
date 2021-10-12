00cf19191ee773d7548b10568d0504b2480e46db
ali <ali.mehry@outlook.com>
2021-10-12 16:20:08 +0330

    Define Meals & AvailableMeals and MealsSummary.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
 
+<main></main>;
 export default App;
